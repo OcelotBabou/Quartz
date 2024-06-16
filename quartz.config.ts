3c64143676e99183168f53bc0351ed302bc18b60
Sam <rulzy999@hotmail.com>
2024-06-16 16:30:31 +1000

    Quartz sync: Jun 16, 2024, 4:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Staterum",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
@@ -22,8 +22,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Cinzel",
+        body: "IM Fell English SC",
         code: "IBM Plex Mono",
       },
       colors: {
